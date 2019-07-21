5c354a37456467455bb63d180d1cf34d1985f53d
Ines Coelho <ines.opcoelho@gmail.com>
2019-07-21 15:33:36 +0100

    tidy up

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 import './index.less';
 
-ReactDOM.render(<><div className={"hello"}>Hello WorldASDDDDDDDDDDD!</div>CZXVXGEZFS</>, document.getElementById('root'));
+ReactDOM.render(<div className={"hello"}>Hello World!</div>, document.getElementById('root'));
